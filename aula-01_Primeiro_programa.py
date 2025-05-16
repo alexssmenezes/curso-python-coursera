2c6ff8b96ffa88d9e17af70a4593c38368e61cb8
Alex Menezes <alex.menezes.one@gmail.com>
2025-05-16 16:12:37 -0300

    Commit aula Python

---
+++ aula-01_Primeiro_programa.py
@@ -0,0 +1,2 @@
+# O primeiro programa em código Python, é simples para imprimir em tela a mensagem solicitada!
+print("Meu primeiro programa em Python!")
---
+++ aula-02_Converter_Bash_para_Python.py
@@ -0,0 +1,4 @@
+# Converter o comando em Bash para Python;
+# echo Have a nice day;
+
+print('Have a nice day')
